0e90242a18545c466c4c598a46e429be2003467f
Olly Sholotan <ollysho@gmail.com>
2017-10-19 12:45:14 -0700

    Added hella basic generation logic
    Also cec logo at the bottom

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
 $(document).ready(function() {
     
+    //open variables
+    var premise = '';
+    var character = '';
+    var verb = '';
+    var closer = '';
+    var genre = '';
+    
+    //Plot Options
+    var premises = [
+        
+    ];
+    
+    var characters = [
+        
+    ];
+    
+    var verbs = [
+        
+    ];
+    
+    var closers = [
+        
+    ];
+    
+    var genres = [
+        
+    ];
+    
+    
     $('#generate-button').click(function() {
         $('#loading').fadeTo('fast', 1);
+        
+        premise = premises[Math.floor(Math. random() * premises.length)];
+        character = characters[Math.floor(Math.random() * characters.length)];
+        verb = verbs[Math.floor(Math.random() * verbs.length)];
+        closer = closers[Math.floor(Math.random() * closers.length)];
+        grenre = grenres[Math.floor(Math.random() * grenres.length)];
         
         setTimeout(function() {
             $('#movie-plot').html("<p>" + "GENERATED" + "</p>")
